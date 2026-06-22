79a35ac15e53782d27db5ab6e703e3bf9e6adbcd
BL-Abhisheak <abhisheak.bhaskaran@bridgelabz.com>
2026-06-22 15:18:17 +0530

    [Abhisheak] added : UC1 - Feet measurement equality

--- src/main/java/com/quantity/QuantitymeasurementappApplication.java
+++ src/main/java/com/quantity/QuantitymeasurementappApplication.java
@@ -6,8 +6,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuantitymeasurementappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuantitymeasurementappApplication.class, args);
-	}
+		public static class Feet {
+			private final double value;
+
+			public Feet(double value) {
+				this.value = value;
+			}
+
+			@Override
+			public boolean equals(Object obj) {
+				if (this == obj) return true;
+				if (obj == null || getClass() != obj.getClass()) return false;
+				Feet feet = (Feet) obj;
+				return Double.compare(this.value, feet.value) == 0;
+			}
+
+			@Override
+			public int hashCode() {
+				return Double.hashCode(value);
+			}
+		}
+
+		public static boolean compareFeet(double a, double b) {
+			return new Feet(a).equals(new Feet(b));
+		}
+
+		public static void main(String[] args) {
+			System.out.println(compareFeet(1.0, 1.0));
+			System.out.println(compareFeet(1.0, 2.0));
+		}
 
 }
