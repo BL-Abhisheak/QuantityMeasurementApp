5971e2597bcdffa9dcd3d50f0c90082b92e233f6
BL-Abhisheak <abhisheak.bhaskaran@bridgelabz.com>
2026-06-22 15:18:17 +0530

    [Abhisheak] added : UC1 - Feet measurement equality

---
+++ src/test/java/com/quantity/QuantitymeasurementappApplicationTests.java
@@ -0,0 +1,45 @@
+package com.quantity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest
+class QuantitymeasurementappApplicationTests {
+
+		@Test
+		public void testEquality_SameValue() {
+			QuantitymeasurementappApplication.Feet a = new QuantitymeasurementappApplication.Feet(1.0);
+			QuantitymeasurementappApplication.Feet b = new QuantitymeasurementappApplication.Feet(1.0);
+			assertTrue(a.equals(b));
+		}
+
+		@Test
+		public void testEquality_DifferentValue() {
+			QuantitymeasurementappApplication.Feet a = new QuantitymeasurementappApplication.Feet(1.0);
+			QuantitymeasurementappApplication.Feet b = new QuantitymeasurementappApplication.Feet(2.0);
+			assertFalse(a.equals(b));
+		}
+
+		@Test
+		public void testEquality_NullComparison() {
+			QuantitymeasurementappApplication.Feet a = new QuantitymeasurementappApplication.Feet(1.0);
+			assertFalse(a.equals(null));
+		}
+
+		@Test
+		public void testEquality_NonNumericInput() {
+			QuantitymeasurementappApplication.Feet a = new QuantitymeasurementappApplication.Feet(1.0);
+			assertFalse(a.equals("1.0"));
+		}
+
+		@Test
+		public void testEquality_SameReference() {
+			QuantitymeasurementappApplication.Feet a = new QuantitymeasurementappApplication.Feet(1.0);
+			assertTrue(a.equals(a));
+		}
+
+}
