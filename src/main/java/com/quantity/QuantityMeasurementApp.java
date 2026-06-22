3951cdc2e79d8bc63252ce90c93a86b8d6c1ccbd
BL-Abhisheak <abhisheak.bhaskaran@bridgelabz.com>
2026-06-22 17:36:13 +0530

    [Abhisheak] added : UC9 - Weight Measurement Equality, Conversion, and Addition

--- src/main/java/com/quantity/QuantityMeasurementApp.java
+++ src/main/java/com/quantity/QuantityMeasurementApp.java
@@ -3,15 +3,23 @@ package com.quantity;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class QuantityMeasurementApp {
 
 	public static void main(String[] args) {
-		QuantityLength a = new QuantityLength(1.0, LengthUnit.FEET);
-		QuantityLength b = new QuantityLength(12.0, LengthUnit.INCH);
-		System.out.println(a.equals(b));
-		System.out.println(a.convertTo(LengthUnit.INCH));
-		System.out.println(a.add(b, LengthUnit.FEET));
-		System.out.println(LengthUnit.FEET.convertToBaseUnit(12.0));
-		System.out.println(LengthUnit.INCH.convertToBaseUnit(12.0));
+		QuantityLength la = new QuantityLength(1.0, LengthUnit.FEET);
+		QuantityLength lb = new QuantityLength(12.0, LengthUnit.INCH);
+		System.out.println(la.equals(lb));
+
+		QuantityWeight wa = new QuantityWeight(1.0, WeightUnit.KILOGRAM);
+		QuantityWeight wb = new QuantityWeight(1000.0, WeightUnit.GRAM);
+		System.out.println(wa.equals(wb));
+
+		QuantityWeight wc = new QuantityWeight(1.0, WeightUnit.KILOGRAM);
+		System.out.println(wc.convertTo(WeightUnit.GRAM));
+
+		QuantityWeight wd = new QuantityWeight(1.0, WeightUnit.KILOGRAM);
+		QuantityWeight we = new QuantityWeight(1000.0, WeightUnit.GRAM);
+		System.out.println(wd.add(we, WeightUnit.GRAM));
 	}
 }
