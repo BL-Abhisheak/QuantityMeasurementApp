e2de5e1fdcb19705181ea5d5355e8d53533aafc0
BL-Abhisheak <abhisheak.bhaskaran@bridgelabz.com>
2026-06-23 15:15:44 +0530

    [Abhisheak] added : UC10 - Generic Quantity Class with Unit Interface for Multi-Category Support

--- src/main/java/com/quantity/QuantityMeasurementApp.java
+++ src/main/java/com/quantity/QuantityMeasurementApp.java
@@ -6,20 +6,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class QuantityMeasurementApp {
 
+	public static <U extends IMeasurable> void demonstrateEquality(Quantity<U> a, Quantity<U> b) {
+		System.out.println(a + " == " + b + " : " + a.equals(b));
+	}
+
+	public static <U extends IMeasurable> void demonstrateConversion(Quantity<U> a, U targetUnit) {
+		System.out.println("convert(" + a + " to " + targetUnit.getUnitName() + ") = " + a.convertTo(targetUnit));
+	}
+
+	public static <U extends IMeasurable> void demonstrateAddition(Quantity<U> a, Quantity<U> b, U targetUnit) {
+		System.out.println("add(" + a + ", " + b + ", " + targetUnit.getUnitName() + ") = " + a.add(b, targetUnit));
+	}
+
 	public static void main(String[] args) {
-		QuantityLength la = new QuantityLength(1.0, LengthUnit.FEET);
-		QuantityLength lb = new QuantityLength(12.0, LengthUnit.INCH);
-		System.out.println(la.equals(lb));
+		Quantity<LengthUnit> la = new Quantity<>(1.0, LengthUnit.FEET);
+		Quantity<LengthUnit> lb = new Quantity<>(12.0, LengthUnit.INCH);
+		demonstrateEquality(la, lb);
+		demonstrateConversion(la, LengthUnit.INCH);
+		demonstrateAddition(la, lb, LengthUnit.FEET);
 
-		QuantityWeight wa = new QuantityWeight(1.0, WeightUnit.KILOGRAM);
-		QuantityWeight wb = new QuantityWeight(1000.0, WeightUnit.GRAM);
-		System.out.println(wa.equals(wb));
+		Quantity<WeightUnit> wa = new Quantity<>(1.0, WeightUnit.KILOGRAM);
+		Quantity<WeightUnit> wb = new Quantity<>(1000.0, WeightUnit.GRAM);
+		demonstrateEquality(wa, wb);
+		demonstrateConversion(wa, WeightUnit.GRAM);
+		demonstrateAddition(wa, wb, WeightUnit.KILOGRAM);
 
-		QuantityWeight wc = new QuantityWeight(1.0, WeightUnit.KILOGRAM);
-		System.out.println(wc.convertTo(WeightUnit.GRAM));
-
-		QuantityWeight wd = new QuantityWeight(1.0, WeightUnit.KILOGRAM);
-		QuantityWeight we = new QuantityWeight(1000.0, WeightUnit.GRAM);
-		System.out.println(wd.add(we, WeightUnit.GRAM));
+		Quantity<LengthUnit> lc = new Quantity<>(1.0, LengthUnit.FEET);
+		Quantity<WeightUnit> wc = new Quantity<>(1.0, WeightUnit.KILOGRAM);
+		System.out.println(lc.equals(wc));
 	}
 }
