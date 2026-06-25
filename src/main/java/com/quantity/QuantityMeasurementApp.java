471c12a7ab0f4cad671013aff4bf254b71243436
BL-Abhisheak <abhisheak.bhaskaran@bridgelabz.com>
2026-06-25 10:43:50 +0530

    Spring Framework Integration - REST Services and JPA

--- src/main/java/com/quantity/QuantityMeasurementApp.java
+++ src/main/java/com/quantity/QuantityMeasurementApp.java
@@ -1,12 +1,6 @@
 package com.quantity;
 
-import com.quantity.controller.QuantityMeasurementController;
-import com.quantity.dto.QuantityDTO;
-import com.quantity.repository.IQuantityMeasurementRepository;
-import com.quantity.repository.QuantityMeasurementDatabaseRepository;
-import com.quantity.service.IQuantityMeasurementService;
-import com.quantity.service.QuantityMeasurementServiceImpl;
-import com.quantity.util.DatabaseInitializer;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -15,20 +9,6 @@ public class QuantityMeasurementApp {
 
 	public static void main(String[] args) {
 
-		DatabaseInitializer.init();
-
-		IQuantityMeasurementRepository repo =
-				new QuantityMeasurementDatabaseRepository();
-
-		IQuantityMeasurementService service =
-				new QuantityMeasurementServiceImpl(repo);
-
-		QuantityMeasurementController controller =
-				new QuantityMeasurementController(service);
-
-		controller.performComparison(
-				new QuantityDTO(1, "FEET", "LENGTH"),
-				new QuantityDTO(12, "INCHES", "LENGTH")
-		);
+		SpringApplication.run(QuantityMeasurementApp.class, args);
 	}
 }
---
+++ src/main/java/com/quantity/model/OperationType.java
@@ -0,0 +1,9 @@
+package com.quantity.model;
+public enum OperationType {
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE,
+    COMPARE,
+    CONVERT
+}
